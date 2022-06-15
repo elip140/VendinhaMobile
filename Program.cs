98d7b4733d41ac650a19bcbf0338da75f197f7bd
elip140 <epcotrim@gmail.com>
2022-06-15 15:01:23 -0300

    c#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.IO;
+
+
+
+
+// Call asynchronous network methods in a try/catch block to handle exceptions.
+HttpClient client = new HttpClient();
+try	
+{
+    HttpResponseMessage response = await client.GetAsync("https://elip140.github.io/VendinhaMobile/Data2.json");
+    response.EnsureSuccessStatusCode();
+    string responseBody = await response.Content.ReadAsStringAsync();
+    // Above three lines can be replaced with new helper method below
+    // string responseBody = await client.GetStringAsync(uri);
+
+    Console.WriteLine(responseBody);
+}
+catch(HttpRequestException e)
+{
+    Console.WriteLine("\nException Caught!");	
+    Console.WriteLine("Message :{0} ",e.Message);
+}
+
