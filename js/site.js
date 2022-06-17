5d6f131320309a395713237091bd575269b268da
elip140 <epcotrim@gmail.com>
2022-06-17 13:24:30 -0300

    categorias

--- js/site.js
+++ js/site.js
@@ -1,5 +1,6 @@
 ﻿function ConverterJSONprod(json="", isString=false)
 {
+    // Recebe um JSON e gera os cards para cada os produtos
     if(!isString)
     {
         json = JSON.stringify(json);
@@ -7,7 +8,6 @@
 
     var arrayObjects = eval(json);
 
-    //alert(json);
 
     for(i=0; i<arrayObjects.length; i++)
     {
@@ -60,10 +60,70 @@
 
         var Lista = document.getElementById("Lista");
         Lista.appendChild(divcol);
-         
-        //alert(arrayObjects[i].ID);
+    }
+}
+
+
+
+function ConverterJSONcat(json="", isString=false)
+{
+    // Recebe um JSON e gera os cards para cada os categorias
+    if(!isString)
+    {
+        json = JSON.stringify(json);
     }
 
-            
-    //document.getElementById("DataTeste").innerHTML = obj.employees[1].firstName + " " + obj.employees[1].lastName;
+    var arrayObjects = eval(json);
+
+
+    for(i=0; i<arrayObjects.length; i++)
+    {
+        /*<div class="col">
+                <div class="card" onclick="myFunction()">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Categoria</h5>
+                    </div>
+                </div>
+            </div>
+        */ 
+        var divcol = document.createElement("div");
+        divcol.classList.add("col");
+
+        var card = document.createElement("div");
+        card.classList.add("card");
+        card.setAttribute("onclick", "ShowProdutos("+arrayObjects[i].Nome+")");
+
+
+        var img = document.createElement("img");
+        img.setAttribute("src", arrayObjects[i].IMG);
+        img.classList.add("card-img-top");
+
+        var cardbody = document.createElement("div");
+        cardbody.classList.add("card-body");
+
+        var prod_nome = document.createElement("h5");
+        prod_nome.classList.add("card-title");
+        prod_nome.appendChild(document.createTextNode(arrayObjects[i].Nome));
+
+
+
+        cardbody.appendChild(prod_nome);
+
+        card.appendChild(img);
+        card.appendChild(cardbody);
+        
+        divcol.appendChild(card);
+
+        var Lista = document.getElementById("Lista");
+        Lista.appendChild(divcol);
+    }
 }
+
+
+function ShowProdutos(Categoria="")
+{
+    window.AppInventor.setWebViewString(Categoria);
+    window.location.assign("aaaaaaaaa"+Categoria);
+}
+
