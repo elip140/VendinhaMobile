6250093d3f3ce65facd6da1dad6552021955057d
elip140 <epcotrim@gmail.com>
2022-06-18 14:06:38 -0300

    a

--- js/Categorias.js
+++ js/Categorias.js
@@ -58,3 +58,17 @@
         Lista.appendChild(divcol);
     }
 }
+
+function ShowProdutos(cat)
+{
+    try {
+        window.AppInventor.getWebViewString(cat);
+    } catch (error) {
+        alert(error);
+    }
+
+    sessionStorage.setItem('GlobalCategoria', cat);
+    
+    window.location.assign("Produtos.html");
+}
+
