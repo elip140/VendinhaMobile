1df34f24d9f87bf57604b2fc5f4d9c58136cde69
elip140 <epcotrim@gmail.com>
2022-06-20 21:02:46 -0300

    a

--- js/Categorias.js
+++ js/Categorias.js
@@ -11,44 +11,18 @@
 
     for(i=0; i<arrayObjects.length; i++)
     {
-        /*<div class="col">
-                <div class="card" onclick="myFunction()">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Categoria</h5>
-                    </div>
-                </div>
-            </div>
-        */ 
-        var divcol = document.createElement("div");
-        divcol.classList.add("col");
+        var cardCat = `<div class="col">
+                            <div class="card" onClick="ShowProdutos('`+arrayObjects[i].nome+`')">
+                                <img src="`+arrayObjects[i].img+`" class="card-img-top" alt="...">
 
-        var card = document.createElement("div");
-        card.classList.add("card");
-        card.setAttribute("onclick", 'ShowProdutos("'+arrayObjects[i].nome+'")');
-
-
-        var img = document.createElement("img");
-        img.setAttribute("src", arrayObjects[i].IMG);
-        img.classList.add("card-img-top");
-
-        var cardbody = document.createElement("div");
-        cardbody.classList.add("card-body");
-
-        var prod_nome = document.createElement("h5");
-        prod_nome.classList.add("card-title");
-        prod_nome.appendChild(document.createTextNode(arrayObjects[i].Nome));
-
-
-        cardbody.appendChild(prod_nome);
-
-        card.appendChild(img);
-        card.appendChild(cardbody);
-        
-        divcol.appendChild(card);
+                                <div class="card-body">
+                                    <h5 class="card-title">`+arrayObjects[i].nome+`</h5>
+                                </div>
+                            </div>
+                        </div>`;
 
         var Lista = document.getElementById("Lista");
-        Lista.appendChild(divcol);
+        Lista.innerHTML += cardCat;
     }
 }
 
