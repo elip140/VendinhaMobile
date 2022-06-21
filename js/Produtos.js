9e24ed374fdbf93ae77eecf95cd6baa905f99fc9
elip140 <epcotrim@gmail.com>
2022-06-21 16:58:42 -0300

    a

--- js/Produtos.js
+++ js/Produtos.js
@@ -50,7 +50,7 @@ function AddProduto(ProdID)
 {
     try {
         $.ajax({
-            url: 'Produtos/'+ProdID+'.json',
+            url: '../produtos/'+ProdID,
             contentType: 'application/json',
             cache: false,
             method: 'GET',
